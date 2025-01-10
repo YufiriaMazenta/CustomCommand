d0fc1911c648d82615b561f56adc24883b6a25c0
Yufiria <xiaoyi357159@gmail.com>
2025-01-10 19:26:41 +0800

    [1.0.3]现在自定义命令可以传入命令参数到动作上

--- src/main/java/pers/yufiria/customCommand/CustomCommand.java
+++ src/main/java/pers/yufiria/customCommand/CustomCommand.java
@@ -10,12 +10,18 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomCommand extends BukkitCommand {
 
     private final Action action;
+    private static final Pattern ARG_PATTERN = Pattern.compile("<arg_(\\d+)>");
 
     public CustomCommand(String name, String permission, List<String> aliases, Action action) {
         super(CommandInfo.builder(name).permission(new PermInfo(permission)).aliases(aliases).build());
@@ -24,10 +30,31 @@ public class CustomCommand extends BukkitCommand {
 
     @Override
     public void execute(@NotNull CommandSender commandSender, @NotNull List<String> args) {
+        Function<String, String> argPreprocessor = (arg) -> {
+            Matcher matcher = ARG_PATTERN.matcher(arg);
+            StringBuilder result = new StringBuilder();
+            while (matcher.find()) {
+                int index = Integer.parseInt(matcher.group(1));
+
+                // 判断 index 是否在替换列表的范围内
+                String replacement;
+                if (index >= 0 && index < args.size()) {
+                    replacement = args.get(index);
+                } else {
+                    replacement = "";  // 如果超出范围，用空字符串替换
+                }
+
+                // 替换为对应的字符串
+                matcher.appendReplacement(result, replacement);
+            }
+
+            matcher.appendTail(result);
+            return result.toString().trim();
+        };
         if (commandSender instanceof Player player) {
-            this.action.run(player, PluginMain.INSTANCE);
+            this.action.run(player, PluginMain.INSTANCE, argPreprocessor);
         } else {
-            this.action.run(null, PluginMain.INSTANCE);
+            this.action.run(null, PluginMain.INSTANCE, argPreprocessor);
         }
     }
 
